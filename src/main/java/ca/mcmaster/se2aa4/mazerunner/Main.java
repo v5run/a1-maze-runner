988718dc4f5ddb8e70dad91ec1b3837a026b2bf5
Varun Pathak <varun11pathak@gmail.com>
2024-01-24 17:44:25 -0500

    Updated walking skeleton (close #17)

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -16,7 +16,8 @@ public class Main {
     // make objects for maze, path classes
 
     public static void main(String[] args) {
-        int argP = 0;
+        int argP = 1;
+        Maze maze = new Maze();
         logger.info("** Starting Maze Runner");
         if (("-i").equals(args[0]) ||("--input").equals(args[0])){
             logger.info("**** Reading the maze from file " + args[1]);
@@ -24,7 +25,7 @@ public class Main {
             try{
                 //make the maze given the second argument (txt file of maze)
                 //have a method where the maze is stored and can be called upon in other scenarios
-                Maze maze = new Maze();
+                
                 //store matrix as string
                 String[] maze_matrix = maze.matrix(args[1]);
             } catch (IOException e){
@@ -41,7 +42,7 @@ public class Main {
         // if p arguments
         try{
             if (("-p").equals(args[2])){
-                Path path = new Path(args[3]); //send in matrix for maze too
+                Path path = new Path(args[3], maze); //send in matrix for maze too
                 String can_path = path.canonical(); //implement class to the string and turn into canonical form
                 path.status(); // return statement to user stating whether path works
             }
@@ -50,12 +51,12 @@ public class Main {
             }
         } catch (ArrayIndexOutOfBoundsException e){
             logger.info("/!\\No -p arguments given/!\\");
-            argP = 1;
+            argP = 0;
         }
 
         // if no p arguments
         try{
-            if (argP == 1){
+            if (argP == 0){
                 logger.info("**** Computing path");
                 Path auto_path = new Path("none");
                 //find the correct path b/c no path was given by the user
